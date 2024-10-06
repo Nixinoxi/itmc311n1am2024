3f8398b6c22cf9ebfed17ef45b15d8f74edcbf61
Nicole B. Castillo <niccastillo@gbox.adnu.edu.ph>
2024-10-06 23:21:40 +0800

    Removed backend and frontend directories

--- backend/apis/notes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'apis_notes'
-
-urlpatterns = [
-    path('', views.NotesAPIView.as_view(), name='notes'),
-    path('<slug:slug>/', views.NotesAPIView.as_view(), name='single-note'),
-]
--- backend/apis/reviewers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'apis_reviewers'
-
-urlpatterns = [
-    path('', views.ReviewersAPIView.as_view(), name='reviewers'),
-    path('<slug:slug>/', views.ReviewersAPIView.as_view(), name='single-reviewer'),
-]
